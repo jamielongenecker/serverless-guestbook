081309288b2f64a3a2cc3a5aa04ed36253efe701
Jamie Longenecker <52465715+jamielongenecker@users.noreply.github.com>
2022-06-28 23:14:30 -0600

    add apiUrl

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://aa216d2b.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
